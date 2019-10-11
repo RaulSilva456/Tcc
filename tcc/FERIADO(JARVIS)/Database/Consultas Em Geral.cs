f9f95b17892d691525c592d903f442381425df13
teste <teste@INFO08>
2019-10-11 16:30:27 -0300

    Ateraçoes

---
+++ tcc/FERIADO(JARVIS)/Database/Consultas Em Geral.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FERIADO_JARVIS_.Database
+{
+    class Consultas_Em_Geral
+    {
+    }
+}
